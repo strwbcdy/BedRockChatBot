918318f810226546e5acad638d08cf07d6b13766
strwbcdy <tlingg30@gmail.com>
2025-03-05 10:40:22 +1030

    changing repo

--- bin.sh
+++ bin.sh
@@ -42,8 +42,8 @@ DISABLE_IPV6="false"
 ALLOWED_SIGN_UP_EMAIL_DOMAINS=""
 BEDROCK_REGION="us-west-2"
 CDK_JSON_OVERRIDE="{}"
-REPO_URL="https://github.com/aws-samples/bedrock-claude-chat.git"
-VERSION="v2"
+REPO_URL="https://github.com/strwbcdy/BedRockChatBot.git"
+VERSION="main"
 
 # Parse command-line arguments for customization
 while [[ "$#" -gt 0 ]]; do
